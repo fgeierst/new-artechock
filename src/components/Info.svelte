e0c12c5a52524714e9ac7ccb593afe31777312db
Florian Geierstanger <florian.geierstanger@pluswerk.ag>
2023-05-21 21:35:41 +0200

    Feat: Infobox component

---
+++ src/components/Info.svelte
@@ -0,0 +1,29 @@
+<script>
+	export let icon = 'ℹ️';
+</script>
+
+<div class="disclaimer">
+	<span>{icon}</span> 
+	<p>
+		<slot />
+	</p>	
+</div>
+
+<style>
+.disclaimer {
+	color: blue;
+	border: 3px dashed blue;
+	padding: 1rem;
+	display: inline-flex;
+	gap: .75rem;
+	align-items: center;
+}
+
+.disclaimer span {
+	font-size: 150%;
+}
+
+.disclaimer p {
+	margin: 0	;
+}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,13 @@
 <script>
 	import Cinema from '../components/Cinema.svelte';
+	import Info from '../components/Info.svelte';
 	import { data } from '../lib/data.js';
 
 </script>
 
 <h1>Munich Movie Showtimes</h1>
-<div class="disclaimer">
-	<span>⚠️</span> 
-	<p>
-		Note this is a prototype with mockup data, visit <a href="https://www.artechock.de/">artechock.de</a> to see the real movie showtimes.
-	</p>	
-</div>
+
+<Info icon="⚠️">Note this is a prototype with mockup data, visit <a href="https://www.artechock.de/">artechock.de</a> to see the real movie showtimes.</Info>
 
 <div class="cinema-list">
 	{#each data[0].cinemas as cinema}
@@ -19,23 +16,5 @@
 </div>
 
 <style>
-	a {
-		color: inherit;
-	}
-	.disclaimer {
-		color: blue;
-		border: 3px dashed blue;
-		padding: 1rem;
-		display: inline-flex;
-		gap: .75rem;
-		align-items: center;
-	}
-
-	.disclaimer span {
-		font-size: 150%;
-	}
-
-	.disclaimer p {
-		margin: 0	;
-	}
+	
 </style>
