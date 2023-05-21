d69a09cea6edcb0eeb952ded84b330ac9427554d
Florian Geierstanger <florian.geierstanger@pluswerk.ag>
2023-05-21 19:41:05 +0200

    Refactor: Extract mockup data to data.js

---
+++ src/lib/data.js
@@ -0,0 +1,473 @@
+export let data = [
+	{
+		date: '2023-05-18',
+		cinemas: [
+			{
+				name: 'ABC',
+				address: 'Herzogstr. 1',
+				phone: '089 / 33 23 00',
+				movies: [
+					{
+						title: 'Das Lehrerzimmer',
+						times: {
+							Monday: [],
+							Tuesday: ['19:30'],
+							Wednesday: ['19:30'],
+							Thursday: ['15:00'],
+							Friday: ['15:00'],
+							Saturday: ['15:00'],
+							Sunday: ['17:15']
+						},
+						reviews: ['artechock-Kritik', 'artechock-Videokritik']
+					},
+					{
+						title: 'Living – Einmal wirklich leben',
+						times: {
+							Monday: ['19:30'],
+							Tuesday: [],
+							Wednesday: ['19:30'],
+							Thursday: ['17:15'],
+							Friday: [],
+							Saturday: ['17:15'],
+							Sunday: ['15:00']
+						},
+						reviews: []
+					},
+					{
+						title: 'Living – Einmal wirklich leben (Living) (OmU)',
+						times: {
+							Monday: ['17:15'],
+							Tuesday: [],
+							Wednesday: ['17:15'],
+							Thursday: [],
+							Friday: [],
+							Saturday: [],
+							Sunday: []
+						},
+						reviews: []
+					}
+				]
+			},
+			{
+				name: 'Arena Filmtheater',
+				address: 'Hans-Sachs-Straße 7',
+				phone: '089 / 260 32 65',
+				movies: [
+					{
+						title: 'Empire of Light (OmU)',
+						times: {
+							Monday: ['16:00'],
+							Tuesday: [],
+							Wednesday: ['18:15'],
+							Thursday: ['13:00'],
+							Friday: [],
+							Saturday: ['18:15'],
+							Sunday: []
+						},
+						reviews: ['artechock-Kritik']
+					},
+					{
+						title: 'Adiós Buenos Aires',
+						times: {
+							Monday: ['20:45'],
+							Tuesday: ['20:45'],
+							Wednesday: ['20:45'],
+							Thursday: ['13:10', '18:45'],
+							Friday: [],
+							Saturday: ['16:50', '20:45'],
+							Sunday: ['20:45']
+						},
+						reviews: ['artechock-Kritik']
+					},
+					{
+						title: 'Adiós Buenos Aires (OmU)',
+						times: {
+							Monday: ['20:45'],
+							Tuesday: [],
+							Wednesday: [],
+							Thursday: [],
+							Friday: ['20:45'],
+							Saturday: [],
+							Sunday: []
+						},
+						reviews: ['artechock-Kritik']
+					},
+					{
+						title: 'Im Taxi mit Madeleine',
+						times: {
+							Monday: [],
+							Tuesday: ['16:50'],
+							Wednesday: [],
+							Thursday: ['15:00'],
+							Friday: ['16:50'],
+							Saturday: ['15:00'],
+							Sunday: ['15:00']
+						},
+						reviews: ['artechock-Kritik']
+					},
+					{
+						title: 'Im Taxi mit Madeleine (Une belle course) (OmU)',
+						times: {
+							Monday: [],
+							Tuesday: [],
+							Wednesday: [],
+							Thursday: [],
+							Friday: ['16:50'],
+							Saturday: [],
+							Sunday: []
+						},
+						reviews: ['artechock-Kritik']
+					},
+					{
+						title: 'Oink',
+						times: {
+							Monday: [],
+							Tuesday: [],
+							Wednesday: [],
+							Thursday: ['15:15'],
+							Friday: [],
+							Saturday: [],
+							Sunday: ['14:40']
+						},
+						reviews: ['artechock-Kritik']
+					},
+					{
+						title: 'Sparta',
+						times: {
+							Monday: [],
+							Tuesday: [],
+							Wednesday: [],
+							Thursday: ['16:45'],
+							Friday: ['18:45'],
+							Saturday: ['18:45'],
+							Sunday: ['18:45']
+						},
+						reviews: ['artechock-Kritik']
+					},
+					{
+						title: 'Living – Einmal wirklich leben (Living) (OmU)',
+						times: {
+							Monday: [],
+							Tuesday: [],
+							Wednesday: [],
+							Thursday: ['20:40'],
+							Friday: ['20:30'],
+							Saturday: ['16:10', '20:30'],
+							Sunday: ['20:30']
+						},
+						reviews: []
+					},
+					{
+						title: 'Der Fuchs',
+						times: {
+							Monday: [],
+							Tuesday: [],
+							Wednesday: ['16:00'],
+							Thursday: [],
+							Friday: [],
+							Saturday: ['18:15'],
+							Sunday: ['18:15']
+						},
+						reviews: ['artechock-Kritik']
+					},
+					{
+						title: 'Lars Eidinger – Sein oder nicht sein',
+						times: {
+							Monday: [],
+							Tuesday: ['16:20'],
+							Wednesday: [],
+							Thursday: [],
+							Friday: [],
+							Saturday: ['14:20'],
+							Sunday: []
+						},
+						reviews: []
+					},
+					{
+						title: 'Sisi & ich',
+						times: {
+							Monday: [],
+							Tuesday: [],
+							Wednesday: [],
+							Thursday: [],
+							Friday: [],
+							Saturday: [],
+							Sunday: ['12:10']
+						},
+						reviews: ['artechock-Kritik']
+					},
+					{
+						title: 'Die Gewerkschafterin',
+						times: {
+							Monday: [],
+							Tuesday: ['16:20'],
+							Wednesday: [],
+							Thursday: [],
+							Friday: [],
+							Saturday: [],
+							Sunday: ['12:30']
+						},
+						reviews: []
+					},
+					{
+						title: 'Die Gewerkschafterin (La syndicaliste) (OmU)',
+						times: {
+							Monday: ['16:20'],
+							Tuesday: [],
+							Wednesday: [],
+							Thursday: [],
+							Friday: [],
+							Saturday: [],
+							Sunday: []
+						},
+						reviews: []
+					},
+					{
+						title: 'Suzume (Suzume no tojimari) (OmeU)',
+						times: {
+							Monday: ['18:10'],
+							Tuesday: ['18:10'],
+							Wednesday: [],
+							Thursday: [],
+							Friday: [],
+							Saturday: [],
+							Sunday: []
+						},
+						reviews: ['artechock-Kritik']
+					},
+					{
+						title: 'No Goodbye',
+						times: {
+							Monday: [],
+							Tuesday: [],
+							Wednesday: [],
+							Thursday: ['17:00 (Isar Festspiele)'],
+							Friday: [],
+							Saturday: [],
+							Sunday: []
+						},
+						reviews: []
+					},
+					{
+						title: 'A Fine Line – Die Gratwanderung (OmU)',
+						times: {
+							Monday: [],
+							Tuesday: [],
+							Wednesday: [],
+							Thursday: ['17:30 (Isar Festspiele)'],
+							Friday: [],
+							Saturday: [],
+							Sunday: []
+						},
+						reviews: []
+					},
+					{
+						title: 'Maren',
+						times: {
+							Monday: [],
+							Tuesday: [],
+							Wednesday: [],
+							Thursday: ['18:30 (Isar Festspiele)'],
+							Friday: [],
+							Saturday: [],
+							Sunday: []
+						},
+						reviews: []
+					},
+					{
+						title: 'Augenblicklich Depression',
+						times: {
+							Monday: [],
+							Tuesday: [],
+							Wednesday: [],
+							Thursday: ['19:00 (Isar Festspiele)'],
+							Friday: [],
+							Saturday: [],
+							Sunday: []
+						},
+						reviews: []
+					},
+					{
+						title: 'Irritieridi',
+						times: {
+							Monday: [],
+							Tuesday: [],
+							Wednesday: [],
+							Thursday: ['19:10 (Isar Festspiele)'],
+							Friday: [],
+							Saturday: [],
+							Sunday: []
+						},
+						reviews: []
+					},
+					{
+						title: 'Something That Sticks (OF)',
+						times: {
+							Monday: [],
+							Tuesday: [],
+							Wednesday: [],
+							Thursday: ['20:00 (Isar Festspiele)'],
+							Friday: [],
+							Saturday: [],
+							Sunday: []
+						},
+						reviews: []
+					}
+				]
+			},
+			{
+				name: 'Astor im Arri',
+				address: 'Türkenstr. 91',
+				phone: '01805/444 966 (0,42 €/min. · Reservierung kostet extra)',
+				movies: [
+					{
+						title: 'Guardians of the Galaxy Vol. 3 (3D)',
+						times: {
+							Monday: ['16:30', '20:00'],
+							Tuesday: ['16:30', '20:00', '20:45'],
+							Wednesday: ['16:30', '20:00'],
+							Thursday: ['13:15', '16:30', '20:00'],
+							Friday: ['16:30', '20:00'],
+							Saturday: ['17:00'],
+							Sunday: ['13:15', '16:30', '20:00', '21:20']
+						},
+						reviews: ['artechock-Kritik']
+					},
+					{
+						title: 'Guardians of the Galaxy Vol. 3',
+						times: {
+							Monday: [],
+							Tuesday: [],
+							Wednesday: [],
+							Thursday: [],
+							Friday: [],
+							Saturday: ['12:45', '21:20'],
+							Sunday: []
+						},
+						reviews: ['artechock-Kritik']
+					},
+					{
+						title: 'Guardians of the Galaxy Vol. 3 (OF)',
+						times: {
+							Monday: [],
+							Tuesday: ['20:45'],
+							Wednesday: [],
+							Thursday: [],
+							Friday: [],
+							Saturday: [],
+							Sunday: []
+						},
+						reviews: ['artechock-Kritik']
+					},
+					{
+						title: 'Im Taxi mit Madeleine',
+						times: {
+							Monday: [],
+							Tuesday: [],
+							Wednesday: [],
+							Thursday: ['14:00'],
+							Friday: [],
+							Saturday: [],
+							Sunday: ['14:00']
+						},
+						reviews: ['artechock-Kritik']
+					},
+					{
+						title: 'Fast & Furious 10',
+						times: {
+							Monday: ['17:15', '20:30'],
+							Tuesday: ['17:00'],
+							Wednesday: ['17:15', '20:30'],
+							Thursday: ['14:10', '17:15', '20:30'],
+							Friday: ['17:15', '20:30'],
+							Saturday: ['14:00', '16:00', '20:30'],
+							Sunday: ['14:15', '17:15', '20:30']
+						},
+						reviews: []
+					},
+					{
+						title: 'Book Club – Ein neues Kapitel',
+						times: {
+							Monday: [],
+							Tuesday: [],
+							Wednesday: [],
+							Thursday: ['16:15', '18:15', '20:45'],
+							Friday: ['16:45', '19:30'],
+							Saturday: ['13:00', '15:45'],
+							Sunday: ['14:15', '18:45']
+						},
+						reviews: ['artechock-Kritik']
+					},
+					{
+						title: 'Don Giovanni (OmU) (Live aus der Metropolitan Opera, New York)',
+						times: {
+							Monday: [],
+							Tuesday: [],
+							Wednesday: [],
+							Thursday: [],
+							Friday: [],
+							Saturday: ['19:00'],
+							Sunday: []
+						},
+						reviews: []
+					}
+				]
+			},
+			{
+				"name": "Astor Cinema Lounge",
+				"address": "Promenadeplatz 2-6",
+				"phone": "089 / 21 20 811",
+				"movies": [
+					{
+						"title": "Book Club – Ein neues Kapitel",
+						"times": {
+							"Monday": ["20:30"],
+							"Tuesday": ["20:30"],
+							"Wednesday": ["21:15"],
+							"Thursday": ["18:00", "20:30"],
+							"Friday": ["18:00", "20:30"],
+							"Saturday": ["17:00", "19:30"],
+							"Sunday": ["17:00", "19:30"]
+						},
+						"reviews": ["artechock-Kritik"]
+					}
+				]
+			},
+			{
+				"name": "Autokino Aschheim",
+				"address": "Münchner Straße 60",
+				"phone": "089 / 90 76 81",
+				"movies": [
+					{
+						"title": "Fast & Furious 10",
+						"times": {
+							"Monday": ["21:30"],
+							"Tuesday": ["21:30"],
+							"Wednesday": ["21:30"],
+							"Thursday": ["21:30"],
+							"Friday": ["21:30"],
+							"Saturday": ["21:45"],
+							"Sunday": ["21:30"]
+						},
+						"reviews": []
+					},
+					{
+						"title": "Guardians of the Galaxy Vol. 3",
+						"times": {
+							"Monday": ["21:30"],
+							"Tuesday": ["21:30"],
+							"Wednesday": ["21:30"],
+							"Thursday": ["21:30"],
+							"Friday": ["21:30"],
+							"Saturday": ["21:45"],
+							"Sunday": ["21:30"]
+						},
+						"reviews": ["artechock-Kritik"]
+					}
+				]
+			}
+		]
+	}
+]
+	
+	
